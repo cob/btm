d8e327e3f0531f07e2abc4c8c4a93ac6cf18c097
João Nelas <joao.nelas@gmail.com>
2018-01-16 13:49:34 +0000

    removes attempt at configuring via OSGi
    Because:
    
    * it was equinox specific
    * the proper way (Config Admin) may need to touch other parts of the
    code
    * we don't really need it

--- btm/src/main/java/bitronix/tm/osgi/Activator.java
+++ btm/src/main/java/bitronix/tm/osgi/Activator.java
@@ -49,74 +49,17 @@ public class Activator implements BundleActivator {
 
 	private final static Logger log = LoggerFactory.getLogger(Activator.class);
 
-    private static final Pattern UNIQUE_NAME = Pattern.compile("^\\s*resource\\.[^\\.]*\\.uniqueName\\s*=\\s*([^\\s]+)\\s*$");
-
     private ServiceRegistration tmRegistration;
 	private ServiceRegistration utRegistration;
 	private Map<String, ServiceRegistration> dsRegistrations;
 
     @Override
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) {
 		dsRegistrations = new HashMap<String, ServiceRegistration>();
-
-		String configArea = context.getProperty("osgi.configuration.area").replace(" ", "%20");
-
-        String btmPropFile = System.getProperty("bitronix.tm.configuration", "bitronix-default-config.properties");
-
-        URI btmConfig = URI.create(configArea + btmPropFile);
-        File cfgFile = new File(btmConfig);
-        if (!cfgFile.exists()) {
-        	cfgFile = new File(btmPropFile);
-        	if (!cfgFile.exists()) {
-        		throw new InitializationException("Configuration file not found: " + btmPropFile);
-        	}
-        }
-
-        FileReader fileReader = new FileReader(cfgFile);
-        Properties btmProperties = new Properties();
-        try {
-        	btmProperties.load(fileReader);
-        }
-        finally {
-        	fileReader.close();
-        }
-
-        System.setProperty("bitronix.tm.configuration", cfgFile.getAbsolutePath());
-
-        File resourceFile = cfgFile;
-        String resourcePropFile = btmProperties.getProperty("bitronix.tm.resource.configuration");
-        if (resourcePropFile != null) {
-        	URI resourceConfig = URI.create(configArea + resourcePropFile);
-        	resourceFile = new File(resourceConfig);
-            if (!resourceFile.exists()) {
-            	resourceFile = new File(resourcePropFile);
-            	if (!resourceFile.exists()) {
-            		throw new InitializationException("Configuration file not found: " + resourcePropFile);
-            	}
-            }
-            System.setProperty("bitronix.tm.resource.configuration", resourceFile.getAbsolutePath());
-        }
 
         TransactionManager tm = TransactionManagerServices.getTransactionManager();
         tmRegistration = context.registerService(TransactionManager.class.getName(), tm, null);
         utRegistration = context.registerService(UserTransaction.class.getName(), tm, null);
-
-        Map<String, Integer> uniqueNameLineNumbers = rankingOfUniqueNameProperties(resourceFile);
-        Map<String, XAResourceProducer> resources = TransactionManagerServices.getResourceLoader().getResources();
-
-        for (Map.Entry<String, XAResourceProducer> me : resources.entrySet()) {
-            Integer ranking = uniqueNameLineNumbers.get(me.getKey());
-            if (ranking == null) {
-                ranking = 1;
-            }
-
-            Dictionary<String, Object> props = new Hashtable<String, Object>();
-            props.put("service.pid", me.getKey());
-            props.put("service.ranking", ranking);
-            props.put("osgi.jndi.serviceName", me.getKey());
-            ServiceRegistration sr = context.registerService(DataSource.class.getName(), me.getValue(), props);
-            dsRegistrations.put(me.getKey(), sr);
-        }
 
         Configuration conf = TransactionManagerServices.getConfiguration();
         log.info(String.format("Started JTA for server ID '%s'.", conf.getServerId()));
@@ -139,20 +82,4 @@ public class Activator implements BundleActivator {
         log.info(String.format("Stopped JTA for server ID '%s'.", conf.getServerId()));
 	}
 
-	private Map<String, Integer> rankingOfUniqueNameProperties(File file) throws FileNotFoundException, IOException
-    {
-        Map<String, Integer> lineNumbers = new HashMap<String, Integer>();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        int ranking = 1;
-        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-            Matcher matcher = UNIQUE_NAME.matcher(line);
-            if (matcher.matches()) {
-                lineNumbers.put(matcher.group(1), ranking);
-                ranking++;
-            }
-        }
-        reader.close();
-
-        return lineNumbers;
-    }
 }
